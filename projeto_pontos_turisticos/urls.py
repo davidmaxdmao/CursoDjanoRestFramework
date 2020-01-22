ab2e212d6d53386123f748e8ba9266709c1c0d35
david max <davidmax.dmao@gmail.com>
2020-01-22 17:51:06 -0300

    api's das app's atracoes e enderecos finalizadas e funcionando

--- projeto_pontos_turisticos/urls.py
+++ projeto_pontos_turisticos/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from pontos_turisticos.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracoesViewSet)
+router.register(r'enderecos', EnderecoViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
