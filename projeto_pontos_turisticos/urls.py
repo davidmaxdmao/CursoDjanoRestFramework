e6e7770e24698c8af276b22a15cc277cc55a0647
david max <davidmax.dmao@gmail.com>
2020-01-23 08:50:40 -0300

    api do app avaliacoes finalizado e funcionando

--- projeto_pontos_turisticos/urls.py
+++ projeto_pontos_turisticos/urls.py
@@ -20,12 +20,14 @@ from pontos_turisticos.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
+from avaliacoes.api.viewsets import AvaliacaoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
+router.register(r'avaliacoes', AvaliacaoViewSet)
 
 
 urlpatterns = [
