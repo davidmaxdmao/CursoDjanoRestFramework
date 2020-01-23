1ff6b75bad83f22a1b3948a9affd840249325ea9
david max <davidmax.dmao@gmail.com>
2020-01-23 13:30:10 -0300

    Correção no serializer do app comentarios

--- comentarios/api/serializers.py
+++ comentarios/api/serializers.py
@@ -4,4 +4,4 @@ from comentarios.models import Comentario
 class ComentarioSerializer(ModelSerializer):
     class Meta:
         model = Comentario
-        fields = ['usuario','comentario','data','aprovado']
+        fields = ['usuario', 'comentario', 'data', 'aprovado']
